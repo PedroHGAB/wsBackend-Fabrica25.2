fb6515b7ccabffce461f84786189d68004d458e7
Piter <pedrogilaab@gmail.com>
2025-09-14 01:22:50 -0300

    ✨ feat: cria entidades Garagem e CarroSalvo.

--- carros/models.py
+++ carros/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Garagem(models.Model):
+    nome = models.CharField("Nome", max_length=30)
+
+    def __str__(self):
+        return self.nome
+
+class CarroSalvo(models.Model):
+    garagem = models.ForeignKey(Garagem,
+                                 on_delete=models.CASCADE,
+                                 related_name="carros"
+                                )
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100)
+    ano = models.IntegerField()
+    preco = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.marca} {self.modelo} ({self.ano}) - R$ {self.preco}"
